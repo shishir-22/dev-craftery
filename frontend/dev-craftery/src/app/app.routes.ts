fde5343e6d4d9daa83c24ffa2464eeffccf29bd3
sawar27769 <sawar.singh@zs.com>
2024-01-27 01:15:58 +0530

    Adding Listing Page

--- frontend/dev-craftery/src/app/app.routes.ts
+++ frontend/dev-craftery/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListingsComponent } from './listings/listings.component';
 
-export const routes: Routes = [{path: '', component: HomeComponent}];
+export const routes: Routes = [{path: '', component: HomeComponent},
+{ path: 'problems', component: ListingsComponent }];
